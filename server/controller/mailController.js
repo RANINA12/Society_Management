6bbbc3159b891864c955ef60b8774631a3ca0d85
NIKUNJ <163564060+RANINA12@users.noreply.github.com>
2025-07-03 14:34:22 +0530

    Update mailController.js

--- server/controller/mailController.js
+++ server/controller/mailController.js
@@ -18,7 +18,7 @@ function sendOtpMail(Email, otp) {
         from: process.env.NODE_MAILER_USER,
         to: Email,
         subject: "One Time Password - Local Handyman",
-        html: `Please keep your OTP confidential and do not share it with anyone. The OTP will be valid for five minutes only. <br><strong>OTP: ${otp}</strong><br><br>Thank you for choosing Local Handyman!<br><br>If you have any questions, please contact us at:<br>Saksham Gupta: saksham.gupta2020@vitbhopal.ac.in.`,
+        html: `Please keep your OTP confidential and do not share it with anyone. The OTP will be valid for five minutes only. <br><strong>OTP: ${otp}</strong><br><br>Thank you for choosing Local Handyman!<br><br>If you have any questions, please contact us at:<br>Nikunj Bisani : nikunjbisani@gmail.com.`,
     };
 
     transporter.sendMail(mailOptions, function (err, success) {
@@ -47,7 +47,7 @@ function sendJobStartOtpMail(Email, otp) {
         from: process.env.NODE_MAILER_USER,
         to: Email,
         subject: "Job Start One Time Password - Local Handyman",
-        html: `Please share this OTP with the handyman once he arives. <br><strong>OTP: ${otp}</strong><br><br>Thank you for choosing Local Handyman!<br><br>If you have any questions, please contact us at:<br>Saksham Gupta: saksham.gupta2020@vitbhopal.ac.in.`,
+        html: `Please share this OTP with the handyman once he arives. <br><strong>OTP: ${otp}</strong><br><br>Thank you for choosing Local Handyman!<br><br>If you have any questions, please contact us at:<br>Nikunj Bisani : nikunjbisani@gmail.com.`,
     };
 
     transporter.sendMail(mailOptions, function (err, success) {
@@ -76,7 +76,7 @@ function sendLoginVerificationMail(Details) {
         from: process.env.NODE_MAILER_USER,
         to: Details.email,
         subject: `Login Successful - Local Handyman`,
-        html: `Dear <i>${Details.name}</i>,<br><br>You have successfully logged in to your account at Local Handyman! Thank you for being a part of our community.<br><br>If you have any questions or concerns, please don't hesitate to reach out to us. We're here to help please contact us at:<br>Saksham Gupta: saksham.gupta2020@vitbhopal.ac.in.<br><br>Best regards,<br>The Local Handyman Team`,
+        html: `Dear <i>${Details.name}</i>,<br><br>You have successfully logged in to your account at Local Handyman! Thank you for being a part of our community.<br><br>If you have any questions or concerns, please don't hesitate to reach out to us. We're here to help please contact us at:<br>Nikunj Bisani : nikunjbisani@gmail.com<br><br>Best regards,<br>The Local Handyman Team`,
     };
 
     transporter.sendMail(mailOptions, function (err, success) {
@@ -105,7 +105,7 @@ function sendTicket(Details) {
         from: process.env.NODE_MAILER_USER,
         to: Details.email,
         subject: `Your Online Event Pass for ${Details.event_name} - Local Handyman✨`,
-        html: `Dear <i>${Details.name}</i>,<br><br>Thank you for registering for ${Details.event_name}! We are excited to have you join us and want to make sure that you have all the information you need to have a great time.<br><br>Your online pass has been generated and is ready for you to use. Please remember to keep this pass with you at all times during the event and do not share it with anyone else.<br><br><strong>Pass Number: ${Details.pass}</strong><br><br>Here are the details of your registration:<br>Name: ${Details.name}<br>Amount Paid: ${Details.price}<br>Address: ${Details.address1} <br> City: ${Details.city} <br> PinCode: ${Details.zip}<br><br>If you have any questions or concerns, please don't hesitate to reach out to us. We're here to help please contact us at:<br>Anurag Singh: 2002anuragksingh@gmail.com<br>Devanshu Yadav: devanshu.yadav2020@vitbhopal.ac.in<br>Saksham Gupta: saksham.gupta2020@vitbhopal.ac.in.<br><br>Best regards,<br>The Local Handyman Team`,
+        html: `Dear <i>${Details.name}</i>,<br><br>Thank you for registering for ${Details.event_name}! We are excited to have you join us and want to make sure that you have all the information you need to have a great time.<br><br>Your online pass has been generated and is ready for you to use. Please remember to keep this pass with you at all times during the event and do not share it with anyone else.<br><br><strong>Pass Number: ${Details.pass}</strong><br><br>Here are the details of your registration:<br>Name: ${Details.name}<br>Amount Paid: ${Details.price}<br>Address: ${Details.address1} <br> City: ${Details.city} <br> PinCode: ${Details.zip}<br><br>If you have any questions or concerns, please don't hesitate to reach out to us. We're here to help please contact us at:<br>Nikunj Bisani : nikunjbisani@gmail.com<br><br>Best regards,<br>The Local Handyman Team`,
     };
 
     transporter.sendMail(mailOptions, function (err, success) {
